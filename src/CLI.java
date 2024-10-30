a850eff730404a1c5007a08a639ca874831dc44f
YassinRamadan1 <163664293+YassinRamadan1@users.noreply.github.com>
2024-10-30 21:12:15 +0300

    Update CLI.java

--- src/CLI.java
+++ src/CLI.java
@@ -1,168 +1,287 @@
-// CLITester.java
-import org.example.CLI;
-import org.junit.jupiter.api.*;
-import java.io.ByteArrayOutputStream;
+package org.example;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.nio.file.Files;
-import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
+public class CLI {
+    private File currentDirectory;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class CLITest {
-    private CLI cli;
-    private ByteArrayOutputStream outputStream;
-    private PrintStream originalOut;
-    private Path testDir;
-
-    @BeforeAll
-    public void setupClass() throws IOException {
-        testDir = Files.createTempDirectory("cliTestDir");
+    public CLI() {
+        this.currentDirectory = new File(System.getProperty("user.dir"));
     }
 
-    @BeforeEach
-    public void setUp() {
-        cli = new CLI();
-        cli.setCurrentDirectory(testDir.toFile());
-        outputStream = new ByteArrayOutputStream();
-        originalOut = System.out;
-        System.setOut(new PrintStream(outputStream));
+    public void setCurrentDirectory(File directory) {
+        this.currentDirectory = directory;
     }
 
-    @AfterEach
-    public void tearDown() {
-        System.setOut(originalOut);
-        outputStream.reset();
+    public void executeCommand(String command) {
+        String[] args = command.split(" ");
+        String cmd = args[0];
+        String Arg = new String();
+        String path = new String();
+
+        for(int i = 1; i < args.length; i++) {
+            Arg = Arg + " " + args[i];
+            path = path + args[i];
+        }
+        switch (cmd) {
+            case "pwd":
+                System.out.println(currentDirectory.getAbsolutePath());
+                break;
+            case "cd":
+                changeDirectory(path);
+                break;
+            case "ls":
+                listDirectory(Arg);
+                break;
+            case "mkdir":
+                createDirectory(Arg);
+                break;
+            case "rmdir":
+                removeDirectory(args);
+                break;
+            case "touch":
+                createFile(args);
+                break;
+            case "rm":
+                deleteFile(args);
+                break;
+            case "cat":
+                displayFileContent(args);
+                break;
+            case ">":
+                writeToFile(args, false);
+                break;
+            case ">>":
+                writeToFile(args, true);
+                break;
+            case "help":
+                displayHelp();
+                break;
+            case "exit":
+                System.out.println("Exiting CLI...");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Command not recognized.");
+        }
     }
 
-    @AfterAll
-    public void cleanup() throws IOException {
-        Files.walk(testDir)
-                .map(Path::toFile)
-                .forEach(File::delete);
+    private void changeDirectory(String path) {
+            File newPath;
+            // Handle Special Cases
+            if (path.equals("..")) {
+                newPath = currentDirectory.getParentFile();
+                if (newPath == null) {
+                    System.out.println("Directory not found.");
+                    return;
+                }
+            } else {
+                newPath = new File(path);
+                if (!newPath.isAbsolute()) {
+                    newPath = new File(currentDirectory, path);
+                }
+            }
+
+            if (newPath.exists() && newPath.isDirectory()) {
+                currentDirectory = newPath;
+
+                return;
+            } else {
+                System.out.println("Directory not found.");
+                return;
+            }
+        }
+
+
+    private void listDirectory(String command) {
+        String[] words = command.split(" ");
+        ArrayList<String> realWords = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            if(words[i].length() > 0)
+                realWords.add(words[i]);
+        }
+        String currentDir = currentDirectory.getAbsolutePath();
+        File directory = new File(currentDir);
+        File[] filesList = directory.listFiles();
+        Arrays.sort(filesList);
+        if(filesList.length == 0){
+            System.out.println("Directory is empty or cannot be read.");
+            return;
+        }
+        if(realWords.size() == 0){
+            for(int i = 0; i < filesList.length; i++)
+                if(!filesList[i].isHidden()){
+                    if(filesList[i].isDirectory())
+                        System.out.println("[DIR] " + filesList[i].getName());
+                    else
+                        System.out.println("[FILE] " + filesList[i].getName());
+                }
+
+        }
+        else if(realWords.size() == 2 && (realWords.get(0).equals("-r") && realWords.get(1).equals("-a")|| realWords.get(0).equals("-a") && realWords.get(1).equals("-r")) || realWords.size() == 1 && realWords.get(0).equals("-ar")){
+            for(int i = filesList.length - 1; i > -1; i--){
+                if(filesList[i].isDirectory())
+                    System.out.println("[DIR] " + filesList[i].getName());
+                else
+                    System.out.println("[FILE] " + filesList[i].getName());
+            }
+        }
+        else if(realWords.size() == 1) {
+            if(realWords.get(0).equals("-r")){
+                for(int i = filesList.length - 1; i > -1; i--)
+                    if(!filesList[i].isHidden()){
+                        if(filesList[i].isDirectory())
+                            System.out.println("[DIR] " + filesList[i].getName());
+                        else
+                            System.out.println("[FILE] " + filesList[i].getName());
+                    }
+            }
+            else if(realWords.get(0).equals("-a")){
+                for(int i = 0; i < filesList.length; i++) {
+                    if (filesList[i].isDirectory())
+                        System.out.println("[DIR] " + filesList[i].getName());
+                    else
+                        System.out.println("[FILE] " + filesList[i].getName());
+                }
+            }
+            else
+                System.out.println("The given command is not recognized as internal or external command");
+        }
+        else
+            System.out.println("The given command is not recognized as internal or external command");
+
     }
 
-    @Test
-    public void testPwd() {
-        cli.executeCommand("pwd");
-        assertTrue(outputStream.toString().contains(testDir.toString().trim()));
+    private void createDirectory(String command) {
+        String[] words = command.split("\"");
+        ArrayList<String> dirs = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            if(i % 2 != 0)
+                dirs.add(words[i]);
+            else {
+                String[] word = words[i].split(" ");
+                for(int j = 0; j < word.length; j++){
+                    if(!word[j].equals(""))
+                        dirs.add(word[j]);
+                }
+            }
+        }
+        for(int i = 0; i < dirs.size(); i++){
+            File Directory = new File(currentDirectory.getAbsolutePath()+ "\\" + dirs.get(i));
+            if(!Directory.mkdir()){
+                if(Directory.exists())
+                    System.out.println("Directory with path: " + dirs.get(i) + " already exists");
+                else if(!Directory.mkdirs())
+                    System.out.println("Directory with path: " + dirs.get(i) + " already exists");
+            }
+        }
     }
 
-    @Test
-    public void testCd() throws IOException {
-        // Create a subdirectory and navigate into it
-        Path subDir = Files.createDirectory(testDir.resolve("subDir"));
-        cli.executeCommand("cd " + subDir.toString());
-        cli.executeCommand("pwd");
-        assertTrue(outputStream.toString().contains("subDir"));
+    private void removeDirectory(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Directory name not specified.");
+            return;
+        }
+        File dir = new File(currentDirectory, args[1]);
+        if (dir.exists() && dir.isDirectory()) {
+            dir.delete();
+        } else {
+            System.out.println("Directory not found.");
+        }
     }
 
-    @Test
-    public void testCdInvalidDirectory() {
-        cli.executeCommand("cd nonExistentDir");
-        assertTrue(outputStream.toString().contains("Directory not found."));
+    private void createFile(String[] args) {
+        if (args.length < 2) {
+            System.out.println("File name not specified.");
+            return;
+        }
+        File file = new File(currentDirectory, args[1]);
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("Error creating file.");
+        }
     }
 
-    @Test
-    public void testLsEmptyDirectory() {
-        cli.executeCommand("ls");
-        assertTrue(outputStream.toString().contains("Directory is empty or cannot be read."));
+    private void deleteFile(String[] args) {
+        if (args.length < 2) {
+            System.out.println("File name not specified.");
+            return;
+        }
+        File file = new File(currentDirectory, args[1]);
+        if (file.exists() && file.isFile()) {
+            file.delete();
+        } else {
+            System.out.println("File not found.");
+        }
     }
 
-    @Test
-    public void testLsWithFiles() throws IOException {
-        // Create a file to list
-        Files.createFile(testDir.resolve("testFile.txt"));
-        cli.executeCommand("ls");
-        assertTrue(outputStream.toString().contains("testFile.txt"));
+    private void displayFileContent(String[] args) {
+        if (args.length < 2) {
+            System.out.println("File name not specified.");
+            return;
+        }
+        File file = new File(currentDirectory, args[1]);
+        try {
+            Files.lines(file.toPath()).forEach(System.out::println);
+        } catch (IOException e) {
+            System.out.println("Error reading file.");
+        }
     }
 
-    @Test
-    public void testLsAll() throws IOException {
-        // Create a hidden file
-        Files.createFile(testDir.resolve(".hiddenFile"));
-        cli.executeCommand("ls -a");
-        assertTrue(outputStream.toString().contains(".hiddenFile"));
+    private void writeToFile(String[] args, boolean append) {
+        if (args.length < 3) {
+            System.out.println("Insufficient arguments.");
+            return;
+        }
+        File file = new File(currentDirectory, args[1]);
+        try (FileWriter fw = new FileWriter(file, append)) {
+            fw.write(args[2]);
+            System.out.println("Write successful.");
+        } catch (IOException e) {
+            System.out.println("Error writing to file.");
+        }
     }
 
-    @Test
-    public void testLsReverseOrder() throws IOException {
-        // Create files in order to check reverse listing
-        Files.createFile(testDir.resolve("AFile.txt"));
-        Files.createFile(testDir.resolve("BFile.txt"));
-        cli.executeCommand("ls -r");
-        String output = outputStream.toString();
-        assertTrue(output.indexOf("BFile.txt") < output.indexOf("AFile.txt"));
+    private void displayHelp() {
+        System.out.println("Available commands:");
+        System.out.println("pwd - print current directory");
+        System.out.println("cd [directory] - change directory");
+        System.out.println("ls - list files in directory");
+        System.out.println("ls -a - list all files, including hidden");
+        System.out.println("ls -r - list files in reverse order");
+        System.out.println("mkdir [name] - create directory");
+        System.out.println("rmdir [name] - remove directory");
+        System.out.println("touch [name] - create file");
+        System.out.println("rm [name] - remove file");
+        System.out.println("cat [file] - display file content");
+        System.out.println("> [file] [text] - overwrite file with text");
+        System.out.println(">> [file] [text] - append text to file");
+        System.out.println("exit - exit the CLI");
+        System.out.println("help - display this help message");
     }
 
-    @Test
-    public void testMkdir() {
-        cli.executeCommand("mkdir newDir");
-        assertTrue(Files.exists(testDir.resolve("newDir")));
-    }
+    public static void main(String[] args) {
+        CLI cli = new CLI();
+        Scanner scanner = new Scanner(System.in);
 
-    @Test
-    public void testRmdir() throws IOException {
-        // Create and remove a directory
-        Path dir = Files.createDirectory(testDir.resolve("toRemove"));
-        cli.executeCommand("rmdir toRemove");
-        assertFalse(Files.exists(dir));
-    }
+        System.out.println("Welcome to the CLI! Type 'help' for available commands.");
 
-    @Test
-    public void testTouch() {
-        cli.executeCommand("touch newFile.txt");
-        assertTrue(Files.exists(testDir.resolve("newFile.txt")));
-    }
-
-    @Test
-    public void testRm() throws IOException {
-        // Create a file and delete it
-        Path file = Files.createFile(testDir.resolve("fileToDelete.txt"));
-        cli.executeCommand("rm fileToDelete.txt");
-        assertFalse(Files.exists(file));
-    }
-
-    @Test
-    public void testCat() throws IOException {
-        // Create a file with content and read it
-        Path file = Files.write(testDir.resolve("fileToRead.txt"), "Hello CLI".getBytes());
-        cli.executeCommand("cat fileToRead.txt");
-        assertTrue(outputStream.toString().contains("Hello CLI"));
-    }
-
-    @Test
-    public void testWriteToFileOverwrite() throws IOException {
-        cli.executeCommand("> writeFile.txt HelloWorld");
-        Path filePath = testDir.resolve("writeFile.txt");
-        assertTrue(Files.readString(filePath).contains("HelloWorld"));
-    }
-
-    @Test
-    public void testWriteToFileAppend() throws IOException {
-        Path filePath = Files.write(testDir.resolve("appendFile.txt"), "Hello".getBytes());
-        cli.executeCommand(">> appendFile.txt World");
-        assertTrue(Files.readString(filePath).contains("HelloWorld"));
-    }
-
-    @Test
-    public void testHelp() {
-        cli.executeCommand("help");
-        assertTrue(outputStream.toString().contains("Available commands:"));
-    }
-
-    @Test
-    public void testInvalidCommand() {
-        cli.executeCommand("nonexistentCommand");
-        assertTrue(outputStream.toString().contains("Command not recognized."));
-    }
-
-    @Test
-    public void testExit() {
-        // Exit terminates program, not typically testable in unit tests.
-        cli.executeCommand("exit");
-        assertTrue(outputStream.toString().contains("Exiting CLI..."));
+        while (true) {
+            System.out.print("CLI> ");
+            String command = scanner.nextLine();
+            cli.executeCommand(command);
+        }
     }
 }
